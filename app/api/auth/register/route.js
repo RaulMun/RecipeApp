f52c62009e4680a7f65145163ab5bedd97baee70
RaulMun <raulmuniz38@gmail.com>
2025-11-20 00:47:34 -0600

    Refactor import paths to use absolute imports for consistency

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,8 +1,8 @@
 import { NextResponse } from 'next/server'
-import { prisma } from '~/lib/prisma'
+import { prisma } from '@/lib/prisma'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import ERROR from '~/lib/error'
+import ERROR from '@/lib/error'
 
 export const POST = async request => {
   try {
