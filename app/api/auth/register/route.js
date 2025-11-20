386d1c6b0f0099c3e0fd9655a4cae016f9b067c9
RaulMun <raulmuniz38@gmail.com>
2025-11-20 01:10:54 -0600

    Fix import path for ERROR utility in authentication routes

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { prisma } from '@/lib/prisma'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
-import ERROR from '@/lib/error'
+import ERROR from '../../../../lib/error'
 
 export const POST = async request => {
   try {
